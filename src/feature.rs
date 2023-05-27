b8f38c0bff73a7b9948d37611cbdc9cc406968ea
Paul Lange <paul.lange@data-experts.de>
2023-05-27 15:36:21 +0200

    fix: remove WithGeometry trait

--- src/feature.rs
+++ src/feature.rs
@@ -1,17 +1,13 @@
 use std::collections::HashMap;
 
-pub trait WithGeometry<T> {
-  fn get_geometry(&self) -> &T;
-}
-
 pub struct Feature<T> {
   pub geometry: T,
 
   pub properties: Option<HashMap<String, String>>,
 }
 
-impl<T> WithGeometry<T> for Feature<T> {
-  fn get_geometry(&self) -> &T {
+impl<T> Feature<T> {
+  pub fn get_geometry(&self) -> &T {
     &self.geometry
   }
 }
